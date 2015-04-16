8dcf8f4cd9b6da5d54f713cb44e19443cdf88091
Steffen <webprosjekt@gmail.com>
2015-04-16 14:15:53 +0200

    Cfg

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$SQL_SERVER = "217.170.207.75";
+$SQL_USERNAME = "";
+$SQL_PASSWORD = "";
+$SQL_DATABASE = "webprosjekt";
+$ROOT_DIR = "http://localhost/Web-Prosjekt";
+
+$WEBPAGE = array(
+    "title" => "Prosjekt Webutvikling"
+);
